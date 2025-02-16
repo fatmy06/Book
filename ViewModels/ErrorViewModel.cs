da1c5bf750c2642313b709aa62b0dd72bfdcffd8
Фатми М <fatima_746@abv.bg>
2025-02-16 21:09:19 +0200

    Add files via upload

---
+++ ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Book.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
